dc601ce660eaccb288c295174f8844d737d312d5
NekR <nekr.fabula@gmail.com>
2020-02-11 15:48:55 +0300

    Palette

---
+++ build/tasks/generate_palette.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const palette = require('@vkontakte/appearance/main.valette/palette');
+
+/**
+ * @param {string} color ahex
+ * @return {string} color цвет в браузерном представлении
+ */
+function resolveColor(color) {
+  if (color.indexOf('#') === 0 && color.length === 9) { // ahex
+    return ahex2rgba(color.replace('#', ''));
+  }
+  return color;
+}
+
+/**
+ * @param {string} ahex цвет в формате ahex: 00ffffff
+ * @param {number} multiplier
+ * @return {string} цвет в формате rgba
+ */
+function ahex2rgba(ahex, multiplier = 1) {
+  const opacity = parseInt(ahex.slice(0, 2), 16) / 255 * multiplier;
+  const colorHex = ahex.slice(2);
+  return opacify(colorHex, opacity);
+}
+
+/**
+ * @param {string} hex цвет в формате hex: ffffff
+ * @param {number} opacity прозрачность в диапазоне [0, 1]
+ * @return {string} цвет в формате rgba
+ */
+function opacify(hex, opacity) {
+  return `rgba(${parseInt(hex.slice(0, 2), 16)}, ${parseInt(hex.slice(2, 4), 16)}, ${parseInt(hex.slice(4), 16)}, ${opacity.toFixed(2)})`;
+}
+
+function generatePalette(options, onReady) {
+    let css = '/* stylelint-disable */\n/*\n* Этот файл сгенерирован автоматически. Не надо править его руками.\n*/\n';
+    css += ':root {\n';
+
+    Object.keys(palette).forEach((colorName) => {
+      css += `  --${colorName}: ${resolveColor(palette[colorName]).toLowerCase()};\n`;
+    });
+    css += '}\n/* stylelint-enable */';
+    fs.writeFileSync(path.resolve(__dirname, options.dir, options.file || palette.css), css);
+    onReady();
+}
+
+module.exports = generatePalette;
---
+++ build/tasks/generate_scheme.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const scheme = require('@vkontakte/appearance/main.valette/scheme');
+const palette = require('@vkontakte/appearance/main.valette/palette');
+
+/**
+ * @param {object} palette палитра цветов
+ * @param {Object} clusterData
+ * @param {string} clusterData.color_identifier
+ * @param {number} clusterData.alpha_multiplier
+ * @return {string} color цвет в браузерном представлении
+ */
+function resolveColor(palette, clusterData) {
+  const color = palette[clusterData.color_identifier];
+  const alphaMultiplier = clusterData.alpha_multiplier ? Number(clusterData.alpha_multiplier) : 1;
+
+  if (color.indexOf('#') === 0 && color.length === 9) { // ahex
+    return ahex2rgba(color.replace('#', ''), alphaMultiplier);
+  } else if (color.indexOf('#') === 0 && clusterData.alpha_multiplier) {
+    return opacify(color.replace('#', ''), alphaMultiplier);
+  }
+  return color;
+}
+
+/**
+ * @param {string} ahex цвет в формате ahex: 00ffffff
+ * @param {number} multiplier
+ * @return {string} цвет в формате rgba
+ */
+function ahex2rgba(ahex, multiplier = 1) {
+  const opacity = parseInt(ahex.slice(0, 2), 16) / 255 * multiplier;
+  const colorHex = ahex.slice(2);
+  return opacify(colorHex, opacity);
+}
+
+/**
+ * @param {string} hex цвет в формате hex: ffffff
+ * @param {number} opacity прозрачность в диапазоне [0, 1]
+ * @return {string} цвет в формате rgba
+ */
+function opacify(hex, opacity) {
+  return `rgba(${parseInt(hex.slice(0, 2), 16)}, ${parseInt(hex.slice(2, 4), 16)}, ${parseInt(hex.slice(4), 16)}, ${opacity.toFixed(2)})`;
+}
+
+function generateScheme(options, onReady) {
+
+  Object.keys(scheme).forEach((schemeId) => {
+    const clusters = scheme[schemeId].colors;
+    let css = '/* stylelint-disable */\n/*\n* Этот файл сгенерирован автоматически. Не надо править его руками.\n*/\n';
+    css += schemeId === 'client_light' ? ':root {\n' : `body[scheme="${schemeId}"] {\n`;
+    Object.keys(clusters).forEach((clusterId) => {
+      css += `  --${clusterId}: ${resolveColor(palette, clusters[clusterId]).toLowerCase()};\n`;
+    });
+    css += '}\n/* stylelint-enable */';
+    fs.writeFileSync(path.resolve(__dirname, options.dir, `${schemeId}.css`), css);
+  });
+  onReady();
+}
+
+module.exports = generateScheme;
