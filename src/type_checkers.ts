8d25a8d9d2ad1077731e0acabb0f16cb7363333c
a.korotaev <a.korotaev@corp.vk.com>
2020-11-05 09:34:13 +0300

    add typeguard to isFunction

--- src/type_checkers.ts
+++ src/type_checkers.ts
@@ -6,6 +6,6 @@ export function isObjectLike(value: any): boolean {
   return typeof value === 'object' && value !== null;
 }
 
-export function isFunction(value: unknown): boolean {
+export function isFunction(value: any): value is ((...args: any[]) => any) {
   return typeof value === 'function';
 }
