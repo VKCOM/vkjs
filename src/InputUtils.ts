e4c2471c4ebc99877a16061c14c53a5231435905
Evgenii Khramkov <ewgeniux@gmail.com>
2020-12-28 14:32:10 +0300

    check canUseDome before calculations

--- src/InputUtils.ts
+++ src/InputUtils.ts
@@ -1,31 +1,39 @@
 import { isIOS, isIPadOS } from './IOSDetections';
+import { canUseDOM } from './dom';
 
 let hasMouse: boolean;
 let hasTouchEvents: boolean;
 let hasHover: boolean;
 let hasTouch: boolean;
 
-if (isIOS && !isIPadOS) {
+if (!canUseDOM) {
   hasMouse = false;
+  hasTouchEvents = false;
   hasHover = false;
-  hasTouchEvents = true;
-  hasTouch = true;
+  hasTouch = false;
 } else {
-  hasTouchEvents = 'ontouchstart' in document;
-  hasTouch = hasTouchEvents ||
-    ('maxTouchPoints' in navigator && navigator.maxTouchPoints > 0);
-
-  if (hasTouch) {
-    const notMobile = !/android|mobile|tablet/i.test(navigator.userAgent);
-
-    hasMouse = window.matchMedia && matchMedia('(pointer)').matches ?
-      matchMedia('(pointer: fine)').matches : notMobile;
-
-    hasHover = hasMouse && (window.matchMedia && matchMedia('(hover)').matches ?
-      matchMedia('(hover: hover)').matches : notMobile);
+   if (isIOS && !isIPadOS) {
+    hasMouse = false;
+    hasHover = false;
+    hasTouchEvents = true;
+    hasTouch = true;
   } else {
-    hasMouse = true;
-    hasHover = true;
+    hasTouchEvents = 'ontouchstart' in document;
+    hasTouch = hasTouchEvents ||
+      ('maxTouchPoints' in navigator && navigator.maxTouchPoints > 0);
+
+    if (hasTouch) {
+      const notMobile = !/android|mobile|tablet/i.test(navigator.userAgent);
+
+      hasMouse = window.matchMedia && matchMedia('(pointer)').matches ?
+        matchMedia('(pointer: fine)').matches : notMobile;
+
+      hasHover = hasMouse && (window.matchMedia && matchMedia('(hover)').matches ?
+        matchMedia('(hover: hover)').matches : notMobile);
+    } else {
+      hasMouse = true;
+      hasHover = true;
+    }
   }
 }
 
