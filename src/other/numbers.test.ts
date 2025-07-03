c51031bc0a929ecbfb9be7fdf658dd88df4c09ae
Mikhail Bodrov <bodrovmiha@yandex.ru>
2025-07-03 18:09:12 +0300

    Update numbers test lint

--- src/other/numbers.test.ts
+++ src/other/numbers.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, test } from '@jest/globals';
 import { formatNumber } from './numbers';
 
 describe('formatNumber', () => {
-  const cases: [number, string][] = [
+  const cases: Array<[number, string]> = [
     [0, '0'],
     [1, '1'],
     [1000, '1 000'],
@@ -11,13 +11,13 @@ describe('formatNumber', () => {
     [1000000.001, '1 000 000,001'],
     [9000009.999, '9 000 009,999'],
     [999.999, '999,999'],
-    [1999.100, '1 999,1'],
-    [-1999.100, '-1 999,1'],
+    [1999.1, '1 999,1'],
+    [-1999.1, '-1 999,1'],
   ];
 
   cases.forEach(([input, result]) => {
     test(`should format ${input} to ${result}`, () => {
       expect(formatNumber(input)).toBe(result);
-    })
+    });
   });
 });
